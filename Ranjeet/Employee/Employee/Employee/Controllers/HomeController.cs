5ba3f299a1cacfe98bc50b90eba31be48822f3c5
lalit <lalitk@clickwt.com>
2015-06-03 18:00:27 +0530

    Interview

---
+++ Ranjeet/Employee/Employee/Employee/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using Employee.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Employee.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private UsersContext db = new UsersContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddEmployee()
+        {
+            ViewBag.Message = "Add Employee.";
+            EmployeeModel objEmployeeModel = new EmployeeModel();
+            FillDropdowns();     
+            return View(objEmployeeModel);
+        }
+
+        private void FillDropdowns()
+        {
+            ViewBag.StateID = db.State.OrderBy(q => q.StateName);
+
+            List<City> objcity = new List<City>();
+            ViewBag.CityID = new SelectList(objcity, "Id", "CityName", 0);
+        }
+
+        [HttpPost]
+        public ActionResult AddEmployee(EmployeeModel employee)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Employee.Add(employee);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                //Log the error (uncomment dex variable name after DataException and add a line here to write a log.)
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+ 
+            }
+            FillDropdowns();
+            return View(employee);
+        }
+
+        public ActionResult AllEmployees()
+        {
+            ViewBag.Message = "All Employees.";
+            return View(db.Employee);
+        }
+
+        [HttpPost]
+        public ActionResult GetCityByStaeId(int stateid)
+        {
+            List<City> objcity = new List<City>();
+            objcity = db.City.Where(m => m.StateId == stateid).ToList();
+            SelectList obgcity = new SelectList(objcity, "Id", "CityName", 0);
+            return Json(obgcity);
+        }
+    }
+}
---
+++ Ranjeet/Employee/Employee/Employee/Models/EmployeeModels.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Web.Mvc;
+
+namespace Employee.Models
+{
+    [Table("Employee")]
+    public class EmployeeModel
+    {
+        [Required]
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Designation { get; set; }
+
+        public int StateID { get; set; }
+
+        public int CityID { get; set; }
+    }
+
+    [Table("State")]
+    public class State
+    {
+        public int Id { get; set; }
+        public string StateName { get; set; }
+
+    }
+
+    [Table("City")]
+    public class City
+    {
+        public int Id { get; set; }
+        public int StateId { get; set; }
+        public string CityName { get; set; }
+                
+    }
+
+}
